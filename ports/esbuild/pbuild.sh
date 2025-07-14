88b4e672555cf33a33493d8da30790c77814d9c5
Seph Gentle <me@josephg.com>
2025-07-14 12:09:51 +1000

    ports/esbuild: Store hash per architecture. Fixes build on x86_64

--- ports/esbuild/pbuild.sh
+++ ports/esbuild/pbuild.sh
@@ -9,13 +9,19 @@ VERSION=0.24.2
 
 # See https://github.com/evanw/esbuild/tree/v0.24.2/npm/%40esbuild
 case "$ARCH" in
-	aarch64) _arch=arm64 ;;
-	x86_64)  _arch=x64 ;;
+	aarch64)
+		_arch=arm64
+		_hash=7df901475cebc48a59a7ea64fcc75c9a95018fc56faa721ccc1f4cbf213f0e16
+		;;
+	x86_64)
+		_arch=x64
+		_hash=aa469e907204bade7c37df6376f7ce3562574e937a61984ba8b89db63e03883a
+		;;
 esac
 
 pbuild_fetch_and_unpack \
 	https://registry.npmjs.org/@esbuild/linux-$_arch/-/linux-$_arch-$VERSION.tgz \
-	7df901475cebc48a59a7ea64fcc75c9a95018fc56faa721ccc1f4cbf213f0e16 \
+	$_hash \
 	esbuild-$_arch-$VERSION.tar.gz
 
 install -v -D -m0755 bin/esbuild $DESTDIR/bin/esbuild
